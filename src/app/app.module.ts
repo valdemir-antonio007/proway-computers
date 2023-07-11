d12af80d340aedc059b647a6d578b40b1bdc9059
Valdemir Antonio <gerente.decorarte@hotmail.com>
2023-07-10 23:45:30 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatSnackBarModule} from "@angular/material/snack-bar"
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +28,8 @@ import { FooterComponent } from './footer/footer.component';
     MatIconModule,
     MatButtonModule,
     MatBadgeModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IProduto } from '../produto';
 import { ProdutoService } from '../produto.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
@@ -11,10 +12,22 @@ export class ProdutosComponent implements OnInit {
 
   produtos: IProduto[] = []
 
-  constructor(private produtoService: ProdutoService){}
+  constructor(private produtoService: ProdutoService, private route: ActivatedRoute){}
 
   ngOnInit(): void {
       this.produtos = this.produtoService.getAll()
+
+      const produtos = this.produtoService.getAll()
+      this.route.queryParamMap.subscribe(params =>{
+          const descricao = params.get("descricao")?.toLowerCase()
+
+          if(descricao){
+             this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao))
+             return;
+          }
+          this.produtos = produtos;
+      })
+
   }
 
 }
